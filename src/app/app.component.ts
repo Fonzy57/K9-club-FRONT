47e149c37bba189316d66434184641d066b822d0
Fonzy57 <traben@hotmail.fr>
2025-03-28 19:34:24 +0100

    Add button component on home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './components/button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'k9-club';
+
+  handleClickPrimary() {
+    console.log('Bouton primary cliqué');
+  }
 }
