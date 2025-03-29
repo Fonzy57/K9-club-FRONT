70cc6debed9d055be84e1447686027e9d18e5522
Fonzy57 <traben@hotmail.fr>
2025-03-29 10:30:44 +0100

    Add size prop for button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -4,6 +4,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 // TODO REVOIR ICI LE TYPAGE POUR QUE MON IDE ME LE PROPOSE
 export type ButtonType = 'primary' | 'ghost';
 // TODO AJOUTER AUSSI UN TYPE POUR LA TAILLE ET UN DISABLED
+export type ButtonSize = 'normal' | 'tiny';
 
 @Component({
   selector: 'app-button',
@@ -15,6 +16,7 @@ export type ButtonType = 'primary' | 'ghost';
 export class ButtonComponent {
   // TODO REVOIR ICI LE TYPAGE POUR QUE MON IDE ME LE PROPOSE
   @Input() type: ButtonType = 'primary';
+  @Input() size: ButtonSize = 'normal';
   @Output() onClick = new EventEmitter<void>();
 
   handleClick() {
