886a8f5c9822d31f09e1f8c5242a0ec636d1ece0
Fonzy57 <traben@hotmail.fr>
2025-03-29 08:28:51 +0100

    Add tagName component

---
+++ src/app/components/tag-name/tag-name.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TagNameComponent } from './tag-name.component';
+
+describe('TagNameComponent', () => {
+  let component: TagNameComponent;
+  let fixture: ComponentFixture<TagNameComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TagNameComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TagNameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/tag-name/tag-name.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-name',
+  imports: [],
+  templateUrl: './tag-name.component.html',
+  styleUrl: './tag-name.component.css'
+})
+export class TagNameComponent {
+
+}
