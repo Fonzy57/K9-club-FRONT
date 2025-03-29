e4651f8c8f2316b6f74d1b5aceee4e6178c3863a
Fonzy57 <traben@hotmail.fr>
2025-03-29 10:21:30 +0100

    Add card course component

---
+++ src/app/components/card-course/card-course.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-course',
+  imports: [],
+  templateUrl: './card-course.component.html',
+  styleUrl: './card-course.component.css'
+})
+export class CardCourseComponent {
+
+}
