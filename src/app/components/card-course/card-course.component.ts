765cc9d34d864e203dc2082865694508152e3844
Fonzy57 <traben@hotmail.fr>
2025-03-29 11:08:52 +0100

    Add card course type and dynamix values

--- src/app/components/card-course/card-course.component.ts
+++ src/app/components/card-course/card-course.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+import { TagNameComponent } from '../tag-name/tag-name.component';
+import { CardCourse } from './card-course.type';
 
 @Component({
   selector: 'app-card-course',
-  imports: [],
+  imports: [ButtonComponent, TagNameComponent],
   templateUrl: './card-course.component.html',
-  styleUrl: './card-course.component.css'
+  styleUrl: './card-course.component.css',
 })
 export class CardCourseComponent {
-
+  @Input() course!: CardCourse;
 }
