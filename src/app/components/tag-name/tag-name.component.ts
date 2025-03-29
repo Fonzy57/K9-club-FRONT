13864821c0e37be78fee23fc73cf28821f178bf5
Fonzy57 <traben@hotmail.fr>
2025-03-29 11:55:43 +0100

    Add mapping for tag type and tag color and tag label

--- src/app/components/tag-name/tag-name.component.ts
+++ src/app/components/tag-name/tag-name.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { TagColor, TagSize } from './tag-name.type';
+import {
+  Tag,
+  TAG_TYPE_COLOR_MAP,
+  TAG_TYPE_LABEL_MAP,
+  TagSize,
+} from './tag-name.type';
 
 @Component({
   selector: 'app-tag-name',
@@ -10,6 +15,17 @@ import { TagColor, TagSize } from './tag-name.type';
 })
 export class TagNameComponent {
   // TODO REVOIR ICI LE TYPAGE POUR QUE MON IDE ME LE PROPOSE
-  @Input() color: TagColor = 'red';
-  @Input() size: TagSize = 'tiny';
+  @Input() tag!: Tag;
+
+  get color(): string {
+    return TAG_TYPE_COLOR_MAP[this.tag.name] || 'gray';
+  }
+
+  get label(): string {
+    return TAG_TYPE_LABEL_MAP[this.tag.name] || this.tag.name;
+  }
+
+  get size(): TagSize {
+    return this.tag.size ?? 'tiny'; // Par défaut, la taille est 'tiny' si non précisée
+  }
 }
