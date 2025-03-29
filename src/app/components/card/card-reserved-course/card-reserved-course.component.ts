e4d5c18559b2d6d4b8e9534c41a4c637eda163ce
Fonzy57 <traben@hotmail.fr>
2025-03-29 12:07:49 +0100

    Add reserved course card component

---
+++ src/app/components/card/card-reserved-course/card-reserved-course.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-reserved-course',
+  imports: [],
+  templateUrl: './card-reserved-course.component.html',
+  styleUrl: './card-reserved-course.component.css'
+})
+export class CardReservedCourseComponent {
+
+}
