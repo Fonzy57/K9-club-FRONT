4fb2dfd5eb8cf52b3961c86699b0beab44fea14a
Fonzy57 <traben@hotmail.fr>
2025-03-29 13:29:54 +0100

    Add card reserved course on home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { TagNameComponent } from './components/tag-name/tag-name.component';
 import { Tag } from './components/tag-name/tag-name.type';
 import { CardCourseComponent } from './components/card/card-course/card-course.component';
 import { CardCourse } from './components/card/card-course/card-course.type';
+import { CardReservedCourseComponent } from './components/card/card-reserved-course/card-reserved-course.component';
+import { ReservedCardCourse } from './components/card/card-reserved-course/card-reserved-course.type';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,7 @@ import { CardCourse } from './components/card/card-course/card-course.type';
     ButtonComponent,
     TagNameComponent,
     CardCourseComponent,
+    CardReservedCourseComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
@@ -85,6 +88,35 @@ export class AppComponent {
       maxPlaces: 10,
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when",
+    },
+  ];
+
+  // TODO VOIR POUR FAIRE UNE CONDITION AVEC DES ELEMENTS NON OBLIGATOIRE
+  // COMME CA UNE SEULE CARD POUR LE COURS RESERVES OU NON
+  reservedCourses: ReservedCardCourse[] = [
+    {
+      name: 'Franchissement d’obstacles',
+      date: '21 octobre 2025 10h30',
+      tag: { name: 'agility' },
+      coach: 'Armand LESIGNAC',
+    },
+    {
+      name: 'Recherche d’objets ou de personne',
+      date: '20 avril 2025 9h45',
+      tag: { name: 'detection' },
+      coach: 'Larmina El Akmar',
+    },
+    {
+      name: 'Sociabilité',
+      date: '18 juin 2025 14h00',
+      tag: { name: 'basic' },
+      coach: 'Hubert BONISSEUR DE LA BATH',
+    },
+    {
+      name: 'Initiation au matériel',
+      date: '17 mars 2025',
+      tag: { name: 'canicross' },
+      coach: 'Raymond Pelletier',
     },
   ];
 }
