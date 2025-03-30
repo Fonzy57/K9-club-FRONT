ef3e6b1f2a9fc5be320ae7de4b760ec34405fe74
Fonzy57 <traben@hotmail.fr>
2025-03-30 10:10:07 +0200

    Add tag name importe for card dog component

--- src/app/components/card/card-dog/card-dog.component.ts
+++ src/app/components/card/card-dog/card-dog.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TagNameComponent } from '../../tag-name/tag-name.component';
 
 @Component({
   selector: 'app-card-dog',
-  imports: [],
+  imports: [TagNameComponent],
   templateUrl: './card-dog.component.html',
-  styleUrl: './card-dog.component.css'
+  styleUrl: './card-dog.component.css',
 })
-export class CardDogComponent {
-
-}
+export class CardDogComponent {}
