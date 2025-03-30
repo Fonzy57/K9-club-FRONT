48b0c003e962f824fbb1d2668e7bae944bd856d1
Fonzy57 <traben@hotmail.fr>
2025-03-30 09:10:05 +0200

    Add card dog component

---
+++ src/app/components/card/card-dog/card-dog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-dog',
+  imports: [],
+  templateUrl: './card-dog.component.html',
+  styleUrl: './card-dog.component.css'
+})
+export class CardDogComponent {
+
+}
