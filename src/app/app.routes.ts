ef81cc2371f293cad409c24ba2bcd006d2ff442e
Fonzy57 <traben@hotmail.fr>
2025-04-01 09:22:56 +0200

    Add routes to router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from '@layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    // TODO AJOUTER LA PAGE QUAND ELLE SERA PRETE
+    /* children: [
+      { path: '', component: HomeComponent },
+    ] */
+  },
+  /* {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent }
+    ]
+  },
+  {
+    path: 'app',
+    component: AppLayoutComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      // d'autres routes protégées ici
+    ]
+  },
+  { path: '**', redirectTo: '' } */
+];
