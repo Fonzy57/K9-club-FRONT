e083e13b6303bcf8d9efe24ced70a433bb5f14a4
Fonzy57 <traben@hotmail.fr>
2025-04-01 14:58:14 +0200

    Add footer on home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from '../../components/button/button.component';
+import { ButtonComponent } from '@components/button/button.component';
+import { CopyrightComponent } from '@components/copyright/copyright.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, CopyrightComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
