3776dcb938d668b952d1f47d7181be7bc054cc2c
Fonzy57 <traben@hotmail.fr>
2025-04-01 19:39:33 +0200

    Add register route to router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from '@layouts/auth-layout/auth-layout.component';
 import { MainLayoutComponent } from '@layouts/main-layout/main-layout.component';
 import { HomeComponent } from '@pages/home/home.component';
+import { RegisterComponent } from '@pages/register/register.component';
 
 export const routes: Routes = [
   {
@@ -8,15 +10,25 @@ export const routes: Routes = [
     component: MainLayoutComponent,
     children: [{ path: '', component: HomeComponent }],
   },
-  /* {
+  {
     path: 'auth',
+    /* pathMatch: 'full',
+    redirectTo: 'connexion', */
     component: AuthLayoutComponent,
     children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent }
-    ]
+      {
+        path: 'inscription',
+        component: RegisterComponent,
+        data: {
+          title: 'Rejoignez la communauté K9 Club',
+          subtitle:
+            'Inscrivez-vous pour accéder à nos services dédiés aux passionnés de chiens.',
+        },
+      },
+      /* { path: 'connexion', component: LoginComponent }, */
+    ],
   },
-  {
+  /* {
     path: 'app',
     component: AppLayoutComponent,
     children: [
