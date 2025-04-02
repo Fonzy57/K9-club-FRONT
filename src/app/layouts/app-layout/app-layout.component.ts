c579ae74e04960d288d831a9b022b98c2a668016
Fonzy57 <traben@hotmail.fr>
2025-04-02 18:47:35 +0200

    Add app layout style

--- src/app/layouts/app-layout/app-layout.component.ts
+++ src/app/layouts/app-layout/app-layout.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { version } from '@config/version';
 
 @Component({
   selector: 'app-app-layout',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app-layout.component.html',
-  styleUrl: './app-layout.component.css'
+  styleUrl: './app-layout.component.css',
 })
 export class AppLayoutComponent {
+  /* TODO VOIR POUR LE COMPOSANT COPYRIGHT ET EN FAIRE QU'UN SEUL PEUT ÊTRE */
+  currentYear: number = new Date().getFullYear();
+  version: string = version.number;
 
+  navItems: any[] = [
+    { name: 'dashboard', label: 'Dasboard', icon: 'home', url: '' },
+    { name: 'dog', label: 'Mes chiens', icon: 'dog', url: '' },
+    { name: 'courses', label: 'Les cours', icon: 'course', url: '' },
+  ];
 }
