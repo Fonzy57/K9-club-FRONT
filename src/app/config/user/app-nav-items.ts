877887c1c41cd159baeb63eaeee7ed5dc2520b42
Fonzy57 <traben@hotmail.fr>
2025-04-03 11:47:35 +0200

    Add config file for user nav items

---
+++ src/app/config/user/app-nav-items.ts
@@ -0,0 +1,5 @@
+export const navItems: any[] = [
+  { name: 'dashboard', label: 'Dasboard', icon: 'home', url: '' },
+  { name: 'dog', label: 'Mes chiens', icon: 'dog', url: '' },
+  { name: 'courses', label: 'Les cours', icon: 'course', url: '' },
+];
