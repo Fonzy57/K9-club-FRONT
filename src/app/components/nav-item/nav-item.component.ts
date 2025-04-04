0e331ed84e6ded16b36a2efecda2177e1e5bb712
Fonzy57 <traben@hotmail.fr>
2025-04-04 19:23:04 +0200

    Add router link active for sidebar menu

--- src/app/components/nav-item/nav-item.component.ts
+++ src/app/components/nav-item/nav-item.component.ts
@@ -1,10 +1,11 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CustomIconComponent } from '@components/custom-icon/custom-icon.component';
 
 @Component({
   selector: 'app-nav-item',
-  imports: [RouterLink, CustomIconComponent],
+  imports: [RouterLink, CustomIconComponent, RouterLinkActive, NgClass],
   templateUrl: './nav-item.component.html',
   styleUrl: './nav-item.component.css',
 })
