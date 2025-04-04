2221664f0d0144e9c9d40f65899687a87ad7f476
Fonzy57 <traben@hotmail.fr>
2025-04-04 18:59:06 +0200

    Add custom icon component to nav item component

--- src/app/components/nav-item/nav-item.component.ts
+++ src/app/components/nav-item/nav-item.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CustomIconComponent } from '@components/custom-icon/custom-icon.component';
 
 @Component({
   selector: 'app-nav-item',
-  imports: [RouterLink],
+  imports: [RouterLink, CustomIconComponent],
   templateUrl: './nav-item.component.html',
   styleUrl: './nav-item.component.css',
 })
 export class NavItemComponent {
   @Input() label: string = '';
   @Input() url: string = '';
+  @Input() iconName: string = '';
 }
