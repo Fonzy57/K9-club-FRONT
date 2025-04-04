128280fdb2123936028073a631e4819f09f7d03f
Fonzy57 <traben@hotmail.fr>
2025-04-04 18:57:40 +0200

    Add CustomIcon component

---
+++ src/app/components/custom-icon/custom-icon.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  ionHomeOutline,
+  ionPawOutline,
+  ionCalendarNumberOutline,
+  ionCalendarClearOutline,
+  ionPersonOutline,
+  ionPencilOutline,
+  ionTrashOutline,
+  ionInformationCircleOutline,
+  ionChevronBackOutline,
+  ionAddOutline,
+  ionSunnyOutline,
+  ionMoonOutline,
+} from '@ng-icons/ionicons';
+
+@Component({
+  selector: 'app-custom-icon',
+  imports: [NgIcon],
+  templateUrl: './custom-icon.component.html',
+  styleUrl: './custom-icon.component.css',
+  viewProviders: [
+    provideIcons({
+      ionHomeOutline,
+      ionPawOutline,
+      ionCalendarNumberOutline,
+      ionCalendarClearOutline,
+      ionPersonOutline,
+      ionPencilOutline,
+      ionTrashOutline,
+      ionInformationCircleOutline,
+      ionChevronBackOutline,
+      ionAddOutline,
+      ionSunnyOutline,
+      ionMoonOutline,
+    }),
+  ],
+})
+export class CustomIconComponent {
+  @Input() name!: string;
+  @Input() size: string = '24';
+}
