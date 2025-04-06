25a3b7f0fb50f59b1880db112afb433fb6428f81
Fonzy57 <traben@hotmail.fr>
2025-04-06 10:02:41 +0200

    Add color changing for places for a course

--- src/app/components/card/card-course/card-course.component.ts
+++ src/app/components/card/card-course/card-course.component.ts
@@ -2,13 +2,28 @@ import { Component, Input } from '@angular/core';
 import { ButtonComponent } from '@components/button/button.component';
 import { TagNameComponent } from '@components/tag-name/tag-name.component';
 import { CardCourse } from './card-course.type';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-card-course',
-  imports: [ButtonComponent, TagNameComponent],
+  imports: [ButtonComponent, TagNameComponent, NgClass],
   templateUrl: './card-course.component.html',
   styleUrl: './card-course.component.css',
 })
 export class CardCourseComponent {
   @Input() course!: CardCourse;
+
+  get placesColor(): string {
+    const placesAvailableInPercent = this.course.places / this.course.maxPlaces;
+
+    if (placesAvailableInPercent > 0.7) {
+      return 'text-error';
+    } else if (
+      placesAvailableInPercent >= 0.5 &&
+      placesAvailableInPercent <= 0.6
+    ) {
+      return 'text-warning';
+    }
+    return 'text-success';
+  }
 }
