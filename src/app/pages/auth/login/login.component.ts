b081d4146e456ca95372a2854ea84a1ef20a60ba
Fonzy57 <traben@hotmail.fr>
2025-04-06 10:42:18 +0200

    Change icon for back button on register and login pages

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -3,10 +3,16 @@ import { RouterModule } from '@angular/router';
 import { ButtonComponent } from '@components/button/button.component';
 import { CustomInputComponent } from '@components/custom-input/custom-input.component';
 import { AppRoutes } from '@config/routes';
+import { CustomIconComponent } from '@components/custom-icon/custom-icon.component';
 
 @Component({
   selector: 'app-login',
-  imports: [ButtonComponent, CustomInputComponent, RouterModule],
+  imports: [
+    ButtonComponent,
+    CustomInputComponent,
+    RouterModule,
+    CustomIconComponent,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -3,10 +3,16 @@ import { RouterModule } from '@angular/router';
 import { ButtonComponent } from '@components/button/button.component';
 import { CustomInputComponent } from '@components/custom-input/custom-input.component';
 import { AppRoutes } from '@config/routes';
+import { CustomIconComponent } from '@components/custom-icon/custom-icon.component';
 
 @Component({
   selector: 'app-register',
-  imports: [ButtonComponent, CustomInputComponent, RouterModule],
+  imports: [
+    ButtonComponent,
+    CustomInputComponent,
+    RouterModule,
+    CustomIconComponent,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
