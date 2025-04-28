857edcc78ca45c5b5033b84a7992e6793d3233f6
Fonzy57 <traben@hotmail.fr>
2025-04-28 13:42:39 +0200

    Add auth service

---
+++ src/app/services/auth/auth-service.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthServiceService {
+  isLogged: boolean = false;
+
+  // TODO REVOIR LE TYPE
+  userInfos: any | null = null;
+
+  constructor() {
+    const jwt: string | null = localStorage.getItem('jwt');
+
+    if (jwt !== null) {
+    }
+  }
+
+  decodeJwt = (jwt: string) => {
+    localStorage.setItem('jwt', jwt);
+
+    const middleOfJwt = jwt.split('.')[1];
+
+    const jwtBody = atob(middleOfJwt);
+
+    const body = JSON.parse(jwtBody);
+
+    this.userInfos = body;
+    this.isLogged = true;
+  };
+
+  logout = () => {
+    localStorage.removeItem('jwt');
+    this.isLogged = false;
+    this.userInfos = null;
+  };
+}
