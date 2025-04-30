8ab3a44cc562a2c7883f395cd508ede7dbcbe8ec
Fonzy57 <traben@hotmail.fr>
2025-04-30 09:40:34 +0200

    Add admin nav items config

---
+++ src/app/config/navigation/admin-nav-items.ts
@@ -0,0 +1,10 @@
+import { AppRoutes } from '@config/routes';
+
+export const adminNavItems: any[] = [
+  {
+    name: 'dash',
+    label: 'ADMIN Dashboard',
+    icon: 'ionHomeOutline',
+    url: AppRoutes.app.admin.dashboardFull,
+  },
+];
