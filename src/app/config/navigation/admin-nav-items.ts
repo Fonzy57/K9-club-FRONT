69937f81f0ccb9219dbeb6d18fff98d4983d96ef
Fonzy57 <traben@hotmail.fr>
2025-05-03 16:19:18 +0200

    Add coachs page to admin nav items

--- src/app/config/navigation/admin-nav-items.ts
+++ src/app/config/navigation/admin-nav-items.ts
@@ -3,8 +3,14 @@ import { AppRoutes } from '@config/routes';
 export const adminNavItems: any[] = [
   {
     name: 'dash',
-    label: 'ADMIN Dashboard',
+    label: 'Dashboard',
     icon: 'ionHomeOutline',
     url: AppRoutes.app.admin.dashboardFull,
+  },
+  {
+    name: 'coaches',
+    label: 'Les coachs',
+    icon: 'ionBodyOutline',
+    url: AppRoutes.app.admin.coachesFull,
   },
 ];
