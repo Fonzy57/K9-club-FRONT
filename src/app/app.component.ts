c723e8745d904a34b41fde56c1efd3bee867e911
Fonzy57 <traben@hotmail.fr>
2025-05-03 12:26:15 +0200

    Add comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,8 @@ import { CardDog } from '@components/card/card-dog/card-dog.type';
 import { PrimeNG } from 'primeng/config';
 import { Toast } from 'primeng/toast';
 
+// TODO voir https://angular.dev/guide/animations pour les animations
+
 @Component({
   selector: 'app-root',
   standalone: true,
