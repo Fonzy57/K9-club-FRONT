47117526199e94520ccb652971d63af2c160e0e7
Fonzy57 <traben@hotmail.fr>
2025-05-04 15:06:38 +0200

    Add form validation for editing or adding coach

--- src/app/pages/app/admin/admin-coach-edit/admin-coach-edit.component.ts
+++ src/app/pages/app/admin/admin-coach-edit/admin-coach-edit.component.ts
@@ -1,11 +1,102 @@
-import { Component } from '@angular/core';
+// ANGULAR
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+// COMPONENTS
+import { CustomInputComponent } from '@components/custom-input/custom-input.component';
+import { ButtonComponent } from '@components/button/button.component';
+
+// VALIDATORS
+import { FormValidators } from 'app/validators/form-validators';
+
+// SERVICES
+import { ToastMessageService } from '@services/toast/toast-message.service';
+
+// CONFIG
+import { AppRoutes } from '@config/routes';
 
 @Component({
   selector: 'app-admin-coach-edit',
-  imports: [],
+  standalone: true,
+  imports: [
+    CustomInputComponent,
+    ButtonComponent,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './admin-coach-edit.component.html',
-  styleUrl: './admin-coach-edit.component.css'
+  styleUrl: './admin-coach-edit.component.css',
 })
 export class AdminCoachEditComponent {
+  AppRoutes = AppRoutes;
+  displayErrors = false;
 
+  http: HttpClient = inject(HttpClient);
+  formBuilder: FormBuilder = inject(FormBuilder);
+  router: Router = inject(Router);
+  toastService: ToastMessageService = inject(ToastMessageService);
+
+  AddOrEditForm = this.formBuilder.group({
+    firstname: ['', FormValidators.nameValidator()],
+    lastname: ['', FormValidators.nameValidator()],
+    email: ['', FormValidators.emailValidator()],
+    /* password:  ['', ValidationUtils.passwordValidators()], */
+  });
+
+  onFieldChange() {
+    this.displayErrors = false;
+  }
+
+  onClick() {
+    if (this.AddOrEditForm.invalid) {
+      this.AddOrEditForm.markAllAsTouched();
+      this.displayErrors = true; // Afficher les erreurs à la soumission
+      return;
+    }
+    console.log("J'ai bien cliqué");
+  }
+
+  get firstnameError() {
+    const control = this.AddOrEditForm.get('firstname');
+
+    if (!control) {
+      return '';
+    }
+
+    if ((control.touched || control.dirty) && control.invalid) {
+      return FormValidators.getNameError(control, 'prénom');
+    }
+
+    return '';
+  }
+
+  get lastnameError() {
+    const control = this.AddOrEditForm.get('lastname');
+
+    if (!control) {
+      return '';
+    }
+
+    if ((control.touched || control.dirty) && control.invalid) {
+      return FormValidators.getNameError(control, 'nom');
+    }
+
+    return '';
+  }
+
+  get emailError() {
+    const control = this.AddOrEditForm.get('email');
+
+    if (!control) {
+      return '';
+    }
+
+    if ((control.touched || control.dirty) && control.invalid) {
+      return FormValidators.getEmailError(control);
+    }
+
+    return '';
+  }
 }
