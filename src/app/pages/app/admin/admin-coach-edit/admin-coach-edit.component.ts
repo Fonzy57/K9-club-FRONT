12c582714adfa36a03cd374eea31b779bd084510
Fonzy57 <traben@hotmail.fr>
2025-05-04 12:57:20 +0200

    Add page for adding or editing coach

---
+++ src/app/pages/app/admin/admin-coach-edit/admin-coach-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-coach-edit',
+  imports: [],
+  templateUrl: './admin-coach-edit.component.html',
+  styleUrl: './admin-coach-edit.component.css'
+})
+export class AdminCoachEditComponent {
+
+}
