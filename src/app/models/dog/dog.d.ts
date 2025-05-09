938040c08c06e3ef1aa28364b830248d38e384f5
Fonzy57 <traben@hotmail.fr>
2025-05-09 16:01:37 +0200

    Fix cardDog type

--- src/app/models/dog/dog.d.ts
+++ src/app/models/dog/dog.d.ts
@@ -10,13 +10,12 @@ interface DogDto {
 
 /* TODO REVOIR CE TYPAGE */
 interface CardDog {
-  image: string;
   name: string;
-  race: string;
+  breed: BreedDto;
   birthday: string;
   nbOfCourses: number;
   nextCourse: string; // TODO CHANGER EN DATE QUAND API OK
   tag: Tag;
-  inscriptionDate: string; // TODO CHANGER EN DATE QUAND API OK
+  createdAt: string;
   badges: BadgeDTO[];
 }
