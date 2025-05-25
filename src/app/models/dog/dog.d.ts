b4331fae52fc614ddb91d114a7031aafb512effc
Fonzy57 <traben@hotmail.fr>
2025-05-25 18:04:16 +0200

    Add next course dto and fix dog dto

--- src/app/models/dog/dog.d.ts
+++ src/app/models/dog/dog.d.ts
@@ -8,15 +8,3 @@ interface DogDto {
   breed: BreedDto;
   registrations: CourseRegistrationDto[];
 }
-
-/* TODO REVOIR CE TYPAGE */
-interface CardDog {
-  name: string;
-  breed: BreedDto;
-  birthdate: string;
-  nbOfCourses: number;
-  nextCourse: string; // TODO CHANGER EN DATE QUAND API OK
-  tag: Tag;
-  createdAt: string;
-  badges: BadgeDTO[];
-}
