f889742717508176597f8125760915431b3b6f5b
Fonzy57 <traben@hotmail.fr>
2025-05-25 16:29:39 +0200

    Fix tag name component

--- src/app/components/tag-name/tag-name.component.ts
+++ src/app/components/tag-name/tag-name.component.ts
@@ -2,34 +2,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
-// TYPES
-import {
-  Tag,
-  TAG_TYPE_COLOR_MAP,
-  TAG_TYPE_LABEL_MAP,
-  TagSize,
-} from './tag-name.type';
-
 @Component({
   selector: 'app-tag-name',
   imports: [CommonModule],
   templateUrl: './tag-name.component.html',
 })
 export class TagNameComponent {
-  // TODO REVOIR ICI LE TYPAGE POUR QUE MON IDE ME LE PROPOSE
-  @Input() tag!: Tag;
-
-  get color(): string {
-    const name = this.tag?.name;
-    return this.tag?.color ?? (name && TAG_TYPE_COLOR_MAP[name]) ?? 'gray';
-  }
-
-  get label(): string {
-    if (!this.tag) {
-      return '';
-    }
-    return TAG_TYPE_LABEL_MAP[this.tag.name] ?? this.tag.name;
-  }
+  @Input() tag!: TagDto;
 
   get size(): TagSize {
     return this.tag?.size ?? 'tiny'; // Par défaut, la taille est 'tiny' si non précisée
