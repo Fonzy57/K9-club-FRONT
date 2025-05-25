1803bb7a58cd696eb0ceda16e3ae95e665bc82ba
Fonzy57 <traben@hotmail.fr>
2025-05-25 16:34:01 +0200

    Change informations from API

--- src/app/components/card/card-dog/card-dog.component.ts
+++ src/app/components/card/card-dog/card-dog.component.ts
@@ -15,5 +15,84 @@ import { AgePipe } from 'app/pipes/age.pipe';
 })
 export class CardDogComponent {
   /* TODO REVOIR TYPAGE */
-  @Input() dog!: CardDog;
+  private _dog!: DogDto;
+
+  nbOfCourses: number = 0;
+  nextCourseName: string = '-';
+  tag: TagDto = {
+    id: 0,
+    name: '',
+    textColor: '',
+    backgroundColor: '',
+  };
+
+  @Input()
+  set dog(value: DogDto) {
+    this._dog = value;
+    this.computeCoursesInfo();
+  }
+
+  get dog() {
+    return this._dog;
+  }
+
+  computeCoursesInfo() {
+    console.log('DOG COMPONENT : ', this._dog);
+
+    if (this._dog.registrations.length === 0) {
+      this.nbOfCourses = 0;
+      this.nextCourseName = '-';
+      this.tag = {
+        id: 0,
+        name: '',
+        textColor: '',
+        backgroundColor: '',
+      };
+      return;
+    }
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    // Number of past courses
+    const pastCourses = this._dog.registrations.filter(
+      (registration: CourseRegistrationDto) => {
+        const courseDate = new Date(registration.course.startDate);
+        return courseDate < today && registration.status === 'CONFIRMED';
+      }
+    );
+
+    this.nbOfCourses = pastCourses.length;
+
+    // Next course confirmed
+    const nextCourse = this._dog.registrations
+      .filter((registration: CourseRegistrationDto) => {
+        const courseDate = new Date(registration.course.startDate);
+        return courseDate > today && registration.status === 'CONFIRMED';
+      })
+      .sort((a: CourseRegistrationDto, b: CourseRegistrationDto) => {
+        return (
+          new Date(a.course.startDate).getTime() -
+          new Date(b.course.startDate).getTime()
+        );
+      });
+
+    if (nextCourse.length > 0) {
+      const next = nextCourse[0];
+
+      this.nextCourseName = next.course.name;
+      this.tag = {
+        ...next.course.courseType,
+        size: 'tiny',
+      };
+    } else {
+      this.nextCourseName = '-';
+      this.tag = {
+        id: 0,
+        name: '',
+        textColor: '',
+        backgroundColor: '',
+      };
+    }
+  }
 }
