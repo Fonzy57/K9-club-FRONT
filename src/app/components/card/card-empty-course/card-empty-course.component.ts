a2f3fd205daefb62ea479c77115040ba0e7ba9f1
Fonzy57 <traben@hotmail.fr>
2025-05-29 15:06:37 +0200

    Add empty course card component

---
+++ src/app/components/card/card-empty-course/card-empty-course.component.ts
@@ -0,0 +1,12 @@
+// ANGULAR
+import { Component } from '@angular/core';
+
+// COMPONENTS
+import { CustomIconComponent } from '../../custom-icon/custom-icon.component';
+
+@Component({
+  selector: 'app-card-empty-course',
+  imports: [CustomIconComponent],
+  templateUrl: './card-empty-course.component.html',
+})
+export class CardEmptyCourseComponent {}
