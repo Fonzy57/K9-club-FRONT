c7845b8a63453e49a95ba7416fbbc6d3bafd5c21
Fonzy57 <traben@hotmail.fr>
2025-05-29 11:57:14 +0200

    Fix card course component with api values

--- src/app/components/card/card-course/card-course.component.ts
+++ src/app/components/card/card-course/card-course.component.ts
@@ -1,24 +1,31 @@
 // ANGULAR
 import { Component, Input } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { DatePipe, NgClass, UpperCasePipe } from '@angular/common';
 
 // COMPONENTS
 import { ButtonComponent } from '@components/button/button.component';
 import { TagNameComponent } from '@components/tag-name/tag-name.component';
 
-// TYPES
-import { CardCourse } from './card-course.type';
-
 @Component({
   selector: 'app-card-course',
-  imports: [ButtonComponent, TagNameComponent, NgClass],
+  imports: [
+    ButtonComponent,
+    TagNameComponent,
+    NgClass,
+    DatePipe,
+    UpperCasePipe,
+  ],
   templateUrl: './card-course.component.html',
 })
 export class CardCourseComponent {
-  @Input() course!: any;
+  @Input() course!: CourseDto;
+
+  get reservedPlaces(): number {
+    return this.course?.registrations?.length ?? 0;
+  }
 
   get placesColor(): string {
-    const placesAvailableInPercent = this.course.places / this.course.maxPlaces;
+    const placesAvailableInPercent = 0 / this.course.maxParticipants;
 
     if (placesAvailableInPercent > 0.7) {
       return 'text-error';
