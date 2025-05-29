6077c67752aab5950472276359ea50e45192a761
Fonzy57 <traben@hotmail.fr>
2025-05-29 10:38:41 +0200

    Change next reserved course card style and pipes

--- src/app/components/card/card-reserved-course/card-reserved-course.component.ts
+++ src/app/components/card/card-reserved-course/card-reserved-course.component.ts
@@ -1,18 +1,16 @@
 // ANGULAR
 import { Component, Input } from '@angular/core';
+import { DatePipe, UpperCasePipe } from '@angular/common';
 
 // COMPONENTS
 import { ButtonComponent } from '@components/button/button.component';
 import { TagNameComponent } from '@components/tag-name/tag-name.component';
 
-// TYPES
-import { ReservedCardCourse } from './card-reserved-course.type';
-
 @Component({
   selector: 'app-card-reserved-course',
-  imports: [ButtonComponent, TagNameComponent],
+  imports: [ButtonComponent, TagNameComponent, DatePipe, UpperCasePipe],
   templateUrl: './card-reserved-course.component.html',
 })
 export class CardReservedCourseComponent {
-  @Input() course!: ReservedCardCourse;
+  @Input() course!: NextReservedCardCourse;
 }
