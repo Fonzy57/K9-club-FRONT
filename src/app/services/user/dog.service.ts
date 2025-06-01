3b35612bd384f579306e5addb4ce8d11629b7d03
Fonzy57 <traben@hotmail.fr>
2025-06-01 16:10:15 +0200

    Add method for adding dog in dog service

--- src/app/services/user/dog.service.ts
+++ src/app/services/user/dog.service.ts
@@ -1,14 +1,14 @@
 // ANGULAR
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { k9Config } from '@config/global';
-import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { k9Config } from "@config/global";
+import { BehaviorSubject } from "rxjs";
 
 // SERVICES
-import { ToastMessageService } from '@services/toast/toast-message.service';
+import { ToastMessageService } from "@services/toast/toast-message.service";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class DogService {
   http: HttpClient = inject(HttpClient);
@@ -17,21 +17,34 @@ export class DogService {
   readonly dogs$ = new BehaviorSubject<DogDto[]>([]);
 
   getAllDogs() {
-    this.http.get<DogDto[]>(k9Config.apiRoot + '/owner/dogs').subscribe({
+    this.http.get<DogDto[]>(k9Config.apiRoot + "/owner/dogs").subscribe({
       next: (dogs) => {
         this.dogs$.next(dogs);
       },
       error: (error) => {
         console.error("Erreur fetching owner's dogs : ", error);
         this.toastService.show({
-          severity: 'error',
-          title: 'Récupération des chiens',
+          severity: "error",
+          title: "Récupération des chiens",
           content:
-            'Une erreur est survenue lors de la récupération des informations de vos chiens.',
+            "Une erreur est survenue lors de la récupération des informations de vos chiens.",
           sticky: true,
         });
       },
     });
+  }
+
+  addDog(dogForm: AddDogFormDto, ownerId: number) {
+    const body = {
+      name: dogForm.name,
+      birthdate: dogForm.birthdate,
+      gender: dogForm.gender,
+      avatarUrl: dogForm.avatarUrl,
+      ownerId: ownerId,
+      breedId: dogForm.breedId,
+    };
+
+    return this.http.post(`${k9Config.apiRoot}/dog`, body);
   }
 
   /* TODO AJOUTER LES AUTRES PARTIES DU CRUD */
