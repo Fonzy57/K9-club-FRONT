1d47b99f871f775aaeaf04e938be37a7f9008dcc
Fonzy57 <traben@hotmail.fr>
2025-06-01 13:27:08 +0200

    Add dog form for owner

--- src/app/pages/app/user/dog/dog.component.ts
+++ src/app/pages/app/user/dog/dog.component.ts
@@ -3,21 +3,36 @@ import { CustomInputComponent } from "@components/custom-input/custom-input.comp
 import { CardWrapperComponent } from "@components/card/card-wrapper/card-wrapper.component";
 import { AppRoutes } from "@config/routes";
 import { HttpClient } from "@angular/common/http";
-import { FormBuilder, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
 import { ToastMessageService } from "@services/toast/toast-message.service";
 import { FormValidators } from "app/validators/form-validators";
 import { k9Config } from "@config/global";
 import { ButtonComponent } from "../../../../components/button/button.component";
+import { DatePickerModule } from "primeng/datepicker";
+import { SelectModule } from "primeng/select";
+import { BreedService } from "@services/breed/breed.service";
+import { BehaviorSubject, Observable } from "rxjs";
+import { CommonModule } from "@angular/common";
+import { RadioButton } from "primeng/radiobutton";
 
 @Component({
   selector: "app-dog",
   imports: [
+    CommonModule,
     CustomInputComponent,
     CardWrapperComponent,
     FormsModule,
     ReactiveFormsModule,
     ButtonComponent,
+    DatePickerModule,
+    SelectModule,
+    RadioButton,
   ],
   templateUrl: "./dog.component.html",
 })
@@ -25,28 +40,40 @@ export class DogComponent {
   AppRoutes = AppRoutes;
   displayErrors = false;
 
-  // -------------------------------------------------------------------------------------------------------
-
-  // ----------------------- TODO JE SUIS LA, GERER LE FORMULAIRE D'AJOUT D'UN CHIEN -----------------------
-
-  // -------------------------------------------------------------------------------------------------------
-
   // Injected services and utilities
   http: HttpClient = inject(HttpClient);
   formBuilder: FormBuilder = inject(FormBuilder);
   router: Router = inject(Router);
   activatedRoute: ActivatedRoute = inject(ActivatedRoute);
   toastService: ToastMessageService = inject(ToastMessageService);
+  breedService: BreedService = inject(BreedService);
 
+  breeds$!: Observable<BreedDto[]>;
+  selectedBreed$ = new BehaviorSubject<BreedDto | undefined>(undefined);
+
+  birthdate: Date | undefined;
+
+  avatars = k9Config.dogAvatars;
+  selectedAvatar: any | undefined = undefined;
+
+  genders = k9Config.dogGender;
+  selectedGender: any;
+
+  /* TODO GERER LES ERREURS */
   addDogForm = this.formBuilder.group({
-    name: ["", FormValidators.nameValidator()],
-    birthdate: ["", FormValidators.emailValidator()],
-    gender: ["", FormValidators.passwordValidator()], // TODO ICI FAIRE UN RADIO BUTTON, CHANGER BOOLEAN DANS BDD ou string
-    breed: [""], // Faire un validateur
-    avatar: [""], // Faire un validateur
+    name: [""],
+    birthdate: [undefined],
+    selectedBreed: [undefined],
+    selectedAvatar: [undefined],
+    gender: [null],
   });
 
-  onClick() {
+  ngOnInit() {
+    this.breedService.getAllBreeds();
+    this.breeds$ = this.breedService.breeds$;
+  }
+
+  onSubmitDogForm() {
     if (this.addDogForm.invalid) {
       this.addDogForm.markAllAsTouched();
       this.displayErrors = true; // Afficher les erreurs à la soumission
@@ -55,11 +82,13 @@ export class DogComponent {
 
     const formValueTrimed: any = {
       name: this.addDogForm.value.name!.trim(),
-      birthdate: this.addDogForm.value.birthdate!.trim(),
-      gender: this.addDogForm.value.gender!.trim(),
-      breed: this.addDogForm.value.breed!.trim(),
+      avatar: this.addDogForm.value.selectedAvatar,
+      gender: this.addDogForm.value.gender,
+      breed: this.addDogForm.value.selectedBreed,
       ownerId: 0, // TODO ici lui passer l'id
     };
+
+    console.log("Form values : ", formValueTrimed);
 
     // We add a new coach
     /* this.http
@@ -92,6 +121,14 @@ export class DogComponent {
   /** Resets the error display flag when any form field value changes */
   onFieldChange() {
     this.displayErrors = false;
+  }
+
+  onSelectBreedChange() {
+    console.log("Race change : ", this.selectedBreed$);
+  }
+
+  onSelectAvatarChange() {
+    console.log("Changement d'avatar : ");
   }
 
   /* TODO REFAIRE LES METHODES POUR AVOIR LES BONNES ERREURS */
