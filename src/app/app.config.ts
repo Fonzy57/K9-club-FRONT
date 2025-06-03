d79d9b682cc5c7e8a06f844ed93852a574a0e0c4
Fonzy57 <traben@hotmail.fr>
2025-06-03 16:39:18 +0200

    Add date picker translation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,22 +4,22 @@ import {
   importProvidersFrom,
   LOCALE_ID,
   provideZoneChangeDetection,
-} from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+} from "@angular/core";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { provideRouter } from "@angular/router";
 
 // INTERCEPTORS
-import { jwtInterceptor } from './interceptos/jwt.interceptor';
+import { jwtInterceptor } from "./interceptos/jwt.interceptor";
 
 // ROUTING
-import { routes } from './app.routes';
+import { routes } from "./app.routes";
 
 // PRIME NG
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
-import { MessageService } from 'primeng/api';
+import { providePrimeNG } from "primeng/config";
+import Aura from "@primeng/themes/aura";
+import { MessageService } from "primeng/api";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -27,7 +27,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([jwtInterceptor])),
     provideAnimationsAsync(),
-    { provide: LOCALE_ID, useValue: 'fr-FR' },
+    { provide: LOCALE_ID, useValue: "fr-FR" },
 
     // TODO SI LIBRAIRIE POSE PROBLEME SUPPRIMER CE QU'IL Y A EN DESSOUS
     // VOIR POUR FAIRE LE DARK MODE
@@ -38,9 +38,54 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura,
         options: {
-          prefix: 'my',
-          darkModeSelector: false || 'none',
+          prefix: "my",
+          darkModeSelector: false || "none",
         },
+      },
+      translation: {
+        dayNames: [
+          "Dimanche",
+          "Lundi",
+          "Mardi",
+          "Mercredi",
+          "Jeudi",
+          "Vendredi",
+          "Samedi",
+        ],
+        dayNamesShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+        dayNamesMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa"],
+        monthNames: [
+          "Janvier",
+          "Février",
+          "Mars",
+          "Avril",
+          "Mai",
+          "Juin",
+          "Juillet",
+          "Août",
+          "Septembre",
+          "Octobre",
+          "Novembre",
+          "Décembre",
+        ],
+        monthNamesShort: [
+          "Jan",
+          "Fév",
+          "Mar",
+          "Avr",
+          "Mai",
+          "Jun",
+          "Jul",
+          "Aoû",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Déc",
+        ],
+        today: "Aujourd'hui",
+        clear: "Effacer",
+        weekHeader: "Sem",
+        firstDayOfWeek: 1, // Lundi = 1, Dimanche = 0
       },
     }),
   ],
