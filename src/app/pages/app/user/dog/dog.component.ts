0396be225ad7fee0ff15cc9e8bffd471e94576a3
Fonzy57 <traben@hotmail.fr>
2025-06-04 10:52:13 +0200

    Change style for dog page

--- src/app/pages/app/user/dog/dog.component.ts
+++ src/app/pages/app/user/dog/dog.component.ts
@@ -17,6 +17,7 @@ import { BehaviorSubject, Observable } from "rxjs";
 import { DatePickerModule } from "primeng/datepicker";
 import { SelectModule } from "primeng/select";
 import { RadioButton } from "primeng/radiobutton";
+import { TabsModule } from "primeng/tabs";
 
 // COMPONENTS
 import { CustomInputComponent } from "@components/custom-input/custom-input.component";
@@ -52,175 +53,28 @@ interface DogForm {
   selector: "app-dog",
   imports: [
     CommonModule,
-    CustomInputComponent,
-    CardWrapperComponent,
     FormsModule,
     ReactiveFormsModule,
-    ButtonComponent,
     DatePickerModule,
     SelectModule,
-    RadioButton,
+    TabsModule,
   ],
   templateUrl: "./dog.component.html",
 })
 export class DogComponent {
-  AppRoutes = AppRoutes;
-  displayErrors = false;
-
-  // Injected services and utilities
-  http: HttpClient = inject(HttpClient);
-  formBuilder: FormBuilder = inject(FormBuilder);
-  router: Router = inject(Router);
-  activatedRoute: ActivatedRoute = inject(ActivatedRoute);
-  toastService: ToastMessageService = inject(ToastMessageService);
-  breedService: BreedService = inject(BreedService);
   userInfoService: UserInfoService = inject(UserInfoService);
   dogService: DogService = inject(DogService);
 
-  breeds$!: Observable<BreedDto[]>;
-  user$ = new BehaviorSubject<UserInfoDto | null>(null);
+  user$!: Observable<UserInfoDto | null>;
+  dogs$!: Observable<DogDto[]>;
 
-  birthdate: Date | undefined;
-
-  avatars = k9Config.dogAvatars;
-  selectedAvatar: DogAvatarDto | undefined = undefined;
-
-  genders = k9Config.dogGender;
-  selectedGender: any;
-
-  addDogForm: FormGroup<DogForm> = this.formBuilder.group({
-    name: new FormControl<string | null>("", FormValidators.dogNameValidator()),
-    selectedAvatar: new FormControl<DogAvatarDto | null>(
-      null,
-      Validators.required
-    ),
-    birthdate: new FormControl<Date | null>(null, [
-      Validators.required,
-      FormValidators.notInFuture(),
-    ]),
-    gender: new FormControl<string | null>(null, Validators.required),
-    selectedBreed: new FormControl<BreedDto | null>(null, Validators.required),
-  });
-
-  maxDate: Date | undefined;
+  nextCourses$!: Observable<NextCourseDto[]>;
 
   ngOnInit() {
-    this.breedService.getAllBreeds();
-    this.breeds$ = this.breedService.breeds$;
-
     this.userInfoService.getUserInfos();
+    this.dogService.getAllDogs();
+
     this.user$ = this.userInfoService.user$;
-
-    /* NOT ALLOWING SELECTION AFTER TODAY FOR DATE PICKER*/
-    const today = new Date();
-    this.maxDate = new Date(today);
-  }
-
-  onSubmitDogForm() {
-    if (this.addDogForm.invalid) {
-      this.addDogForm.markAllAsTouched();
-      this.displayErrors = true;
-      return;
-    }
-
-    const userId = this.userInfoService.user$.getValue()?.id;
-    if (!userId) {
-      this.toastService.show({
-        severity: "error",
-        title: "Erreur",
-        content: "Impossible de récupérer votre identifiant utilisateur.",
-        sticky: true,
-      });
-      return;
-    }
-
-    const formValue = this.addDogForm.value;
-
-    const birthdate = formValue.birthdate
-      ? DateUtils.toLocalDateString(formValue.birthdate)
-      : "";
-
-    const dogData: AddDogFormDto = {
-      name: formValue.name!.trim(),
-      avatarUrl: formValue.selectedAvatar!.url,
-      birthdate: birthdate,
-      gender: formValue.gender!,
-      breedId: formValue.selectedBreed!.id,
-      ownerId: userId,
-    };
-
-    this.dogService.addDog(dogData, userId).subscribe({
-      next: (response) => {
-        this.toastService.show({
-          severity: "success",
-          title: "Succès",
-          content: "Chien ajouté avec succès!",
-        });
-        // TODO FAIRE REDIRECTION
-      },
-      error: (error) => {
-        console.error("Erreur lors de l'ajout du chien:", error);
-        this.toastService.show({
-          severity: "error",
-          title: "Erreur",
-          content: "Erreur lors de l'ajout du chien.",
-          sticky: true,
-        });
-      },
-    });
-  }
-
-  /** Resets the error display flag when any form field value changes */
-  onFieldChange() {
-    this.displayErrors = false;
-  }
-
-  onSelectDate() {
-    this.displayErrors = false;
-  }
-
-  get nameError() {
-    const control = this.addDogForm.get("name");
-    if (!control) return "";
-    if ((control.touched || control.dirty) && control.invalid) {
-      return FormValidators.getDogNameError(control);
-    }
-    return "";
-  }
-
-  get avatarError() {
-    const control = this.addDogForm.get("selectedAvatar");
-    if (!control) return "";
-    if ((control.touched || control.dirty) && control.invalid) {
-      return "Veuillez sélectionner un avatar.";
-    }
-    return "";
-  }
-
-  get birthdateError() {
-    const control = this.addDogForm.get("birthdate");
-    if (!control) return "";
-    if ((control.touched || control.dirty) && control.invalid) {
-      return FormValidators.getBirthdateError(control);
-    }
-    return "";
-  }
-
-  get genderError() {
-    const control = this.addDogForm.get("gender");
-    if (!control) return "";
-    if ((control.touched || control.dirty) && control.invalid) {
-      return "Veuillez sélectionner le genre de votre chien.";
-    }
-    return "";
-  }
-
-  get breedError() {
-    const control = this.addDogForm.get("selectedBreed");
-    if (!control) return "";
-    if ((control.touched || control.dirty) && control.invalid) {
-      return "Veuillez sélectionner une race.";
-    }
-    return "";
+    this.dogs$ = this.dogService.dogs$;
   }
 }
