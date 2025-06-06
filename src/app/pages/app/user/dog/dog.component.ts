250bb6fb9f569c39a70f5a94a1a8c20da3cfa053
Fonzy57 <traben@hotmail.fr>
2025-06-06 14:28:01 +0200

    Add content of dog info card

--- src/app/pages/app/user/dog/dog.component.ts
+++ src/app/pages/app/user/dog/dog.component.ts
@@ -1,8 +1,9 @@
 // ANGULAR
 import { Component, inject } from "@angular/core";
-import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormControl } from "@angular/forms";
 import { CommonModule, DatePipe } from "@angular/common";
 import { Observable } from "rxjs";
+import { RouterLink } from "@angular/router";
 
 // PRIME NG
 import { DatePickerModule } from "primeng/datepicker";
@@ -12,10 +13,15 @@ import { ButtonModule } from "primeng/button";
 
 // COMPONENTS
 import { ButtonComponent } from "@components/button/button.component";
+import { CardEmptyDogComponent } from "@components/card/card-empty-dog/card-empty-dog.component";
+import { CustomIconComponent } from "@components/custom-icon/custom-icon.component";
 
 // SERVICES
 import { UserInfoService } from "@services/user/user-info.service";
 import { DogService } from "@services/user/dog.service";
+
+// PIPES
+import { AgePipe } from "app/pipes/age.pipe";
 
 // CONFIG
 import { AppRoutes } from "@config/routes";
@@ -42,6 +48,10 @@ interface DogForm {
     DatePipe,
     ButtonModule,
     ButtonComponent,
+    CardEmptyDogComponent,
+    CustomIconComponent,
+    RouterLink,
+    AgePipe,
   ],
   templateUrl: "./dog.component.html",
 })
@@ -61,13 +71,85 @@ export class DogComponent {
 
     this.user$ = this.userInfoService.user$;
     this.dogs$ = this.dogService.dogs$;
-
-    /* TODO SUPPRIMER APRES TESTS */
-    this.dogs$.subscribe({
-      next: (response) => {
-        console.log("response : ", response);
-        return response;
-      },
-    });
   }
+
+  daysToNextBirthday(birthdate: string): number {
+    const birthday = new Date(birthdate);
+
+    const today = new Date();
+
+    // Create next birthday date for current year
+    const nextBirthday = new Date(
+      today.getFullYear(),
+      birthday.getMonth(),
+      birthday.getDate()
+    );
+
+    // If this year's birthday has already passed,
+    // calculate for next year
+    if (nextBirthday < today) {
+      nextBirthday.setFullYear(today.getFullYear() + 1);
+    }
+
+    // Calculate difference in milliseconds
+    const diffTime = nextBirthday.getTime() - today.getTime();
+
+    // Convert to days (1 day = 24 * 60 * 60 * 1000 ms)
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    return diffDays;
+  }
+
+  onConfirmDelete(dog: any) {
+    console.log("Je supprime le chien : ", dog.id, dog.name);
+    /* this.confirmationService.confirm({
+        header: "Suppression d'un compte",
+        message: `${coach.firstname} ${coach.lastname.toUpperCase()}`,
+        accept: () => {
+          // DELETING THE COACH
+          this.onDeleteCoach(coach);
+        },
+        reject: () => {
+          this.toastService.show({
+            severity: 'info',
+            title: 'Suppression annulée',
+            content: `Le compte du coach ${
+              coach.firstname
+            } ${coach.lastname.toUpperCase()} n'a pas été supprimé.`,
+            time: 4000,
+          });
+        },
+      }); */
+  }
+
+  /* TODO ICI FONCTION QUE J'AVAIS DANS ADMIN COACH */
+  /* onDeleteDog(dog: any) {
+      this.http
+        .delete<any>(`${k9Config.apiRoot}/coach/${coach.id}`)
+        .subscribe({
+          next: () => {
+            this.coaches = this.coaches.filter((c) => c.id !== coach.id);
+  
+            this.toastService.show({
+              severity: 'success',
+              title: 'Coach supprimé',
+              content: `Le compte du coach ${
+                coach.firstname
+              } ${coach.lastname.toUpperCase()} a bien été supprimé.`,
+              time: 3000,
+            });
+          },
+          error: (err) => {
+            console.error('Erreur lors de la suppression :', err);
+            this.toastService.show({
+              severity: 'error',
+              title: 'Suppression échouée',
+              content: `Une erreur est survenue lors de la suppression du coach ${
+                coach.firstname
+              } ${coach.lastname.toUpperCase()}.`,
+              sticky: true,
+            });
+          },
+        });
+    } */
 }
