f31fdb83071168014546215fe7bfd49e3930b244
Fonzy57 <traben@hotmail.fr>
2025-06-11 12:48:16 +0200

    Add event emitter for cancel registration

--- src/app/components/card/card-reserved-course/card-reserved-course.component.ts
+++ src/app/components/card/card-reserved-course/card-reserved-course.component.ts
@@ -1,16 +1,24 @@
 // ANGULAR
-import { Component, Input } from '@angular/core';
-import { DatePipe, UpperCasePipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { DatePipe, UpperCasePipe } from "@angular/common";
 
 // COMPONENTS
-import { ButtonComponent } from '@components/button/button.component';
-import { TagNameComponent } from '@components/tag-name/tag-name.component';
+import { ButtonComponent } from "@components/button/button.component";
+import { TagNameComponent } from "@components/tag-name/tag-name.component";
 
 @Component({
-  selector: 'app-card-reserved-course',
+  selector: "app-card-reserved-course",
   imports: [ButtonComponent, TagNameComponent, DatePipe, UpperCasePipe],
-  templateUrl: './card-reserved-course.component.html',
+  templateUrl: "./card-reserved-course.component.html",
 })
 export class CardReservedCourseComponent {
-  @Input() course!: NextReservedCardCourseDto;
+  @Input() course!: any;
+
+  @Output() cancelCourse = new EventEmitter<any>();
+
+  onCancelClick() {
+    /* TODO SUPPRIMER APRES TESTS */
+    console.log("Je clique sur cancel", this.course.id, this.course);
+    this.cancelCourse.emit(this.course.id);
+  }
 }
